49ad835fd971abcd60105721bf769bf536af7028
GringoXY <przemyslawkowalski01pk@gmail.com>
2025-05-20 22:31:25 +0200

    add localization setup on startup

--- Infrastructure/Program.cs
+++ Infrastructure/Program.cs
@@ -1,9 +1,15 @@
+using System.Globalization;
 using Infrastructure.Extensions;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.Configure(builder.Configuration);
 
 builder.Configuration.Prepare();
+
+var cultureName = Environment.GetEnvironmentVariable("ASPNETCORE_CULTURE") ?? CultureInfo.CurrentCulture.Name;
+var culture = new CultureInfo(cultureName);
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
 
 var host = builder.Build();
 host.Configure();
